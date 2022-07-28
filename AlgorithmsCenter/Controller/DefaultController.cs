a7ae96e0165e7becb5586482325990692ee5d957
dzyzus <dzyzusek@gmail.com>
2022-07-28 22:37:40 +0200

    Improving app
    * added more messages
    * create controller for catching user commands

--- AlgorithmsCenter/Controller/DefaultController.cs
+++ AlgorithmsCenter/Controller/DefaultController.cs
@@ -1,11 +1,23 @@
 ﻿#region Usings
 using AlgorithmsCenter.MessageTemplate;
+using System;
 #endregion
 
 namespace AlgorithmsCenter.Controller
 {
     public class DefaultController : CustomMessagesTemplate
     {
+        #region Fields and variables
+
+        /// <summary>
+        /// if true, program is still runing
+        /// </summary>
+        private bool loopContinue = true;
+
+        #endregion
+
+        #region Constructor
+
         /// <summary>
         /// Object constructor
         /// </summary>
@@ -13,6 +25,8 @@ namespace AlgorithmsCenter.Controller
         {
             //Constructor
         }
+
+        #endregion
 
         /// <summary>
         /// Primary run app method
@@ -22,6 +36,40 @@ namespace AlgorithmsCenter.Controller
             CopyrightsInfo();
             WelcomeMessage();
             BasicInfo();
+            BasicCommands();
+        }
+        /// <summary>
+        /// Catching basic commands
+        /// </summary>
+        protected override void BasicCommands()
+        {
+            base.BasicCommands();
+            Console.Write("Type command: ");
+            input = Console.ReadLine();
+            while (loopContinue)
+            {
+                switch (input)
+                {
+                    case "-help":
+                        HelpInfo();
+                        Console.Write("Type command: ");
+                        input = Console.ReadLine();
+                        break; ;
+                    case "-description":
+                        AppDescription();
+                        Console.Write("Type command: ");
+                        input = Console.ReadLine();
+                        break;
+                    case "-quit":
+                        loopContinue = false;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect command!\n");
+                        Console.Write("Type command: ");
+                        input = Console.ReadLine();
+                        break; ;
+                }
+            }
         }
     }
 }
--- AlgorithmsCenter/Templates/CustomMessagesTemplate.cs
+++ AlgorithmsCenter/Templates/CustomMessagesTemplate.cs
@@ -11,30 +11,27 @@ namespace AlgorithmsCenter.MessageTemplate
         /// <summary>
         ///  List of all algorithms which app contains.
         /// </summary>
-        List<string> AlogirthmsList = new List<string>();
+        protected List<string> AlogirthmsList = new List<string>();
 
         /// <summary>
-        ///  Get current background console color.
+        ///  Input for selected command.
         /// </summary>
-        ConsoleColor background = Console.BackgroundColor;
-
-        /// <summary>
-        ///  Get current foreground console color.
-        /// </summary>
-        ConsoleColor foreground = Console.ForegroundColor;
+        protected string input = "";
 
         #endregion
 
         #region Methods
         /// <summary>
-        ///  Copyrights info
+        ///  Copyright info
         /// </summary>
         protected void CopyrightsInfo()
         {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("*******************************************************************\n" +
                 "**************** APP AND SOURCE CODE IS FULLY FREE ****************\n" +
                 "********************** AUTHOR: MACIEJ MARZEC **********************\n" +
                 "*******************************************************************");
+            Console.ResetColor();
         }
 
         /// <summary>
@@ -59,12 +56,46 @@ namespace AlgorithmsCenter.MessageTemplate
 
             PrintList(this.AlogirthmsList);
         }
+
+        /// <summary>
+        ///  Information with more specific commands
+        /// </summary>
+        protected void HelpInfo()
+        {
+            Console.WriteLine("Test help");
+        }
+
+        /// <summary>
+        ///  Information about app
+        /// </summary>
+        protected void AppDescription()
+        {
+            Console.WriteLine("Test description");
+        }
+        /// <summary>
+        ///  Information about basic commands
+        /// </summary>
+        protected virtual void BasicCommands()
+        {
+            Console.WriteLine("*******************************************************************\n" +
+                "-help for more information\n" +
+                "-description for more information about app\n" +
+                "-quit for exit app\n" +
+                "*******************************************************************");
+        }
+        /// <summary>
+        ///  Display list of available algorithms
+        /// </summary>
         private void PrintList(List<string> listToPrint)
         {
             if(listToPrint.Count == 0)
             {
-                Console.Write("************* There is none algorithms implemented yet ************\n" +
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("*******************************************************************\n" +
+                    "************* There is none algorithms implemented yet ************\n" +
                     "*******************************************************************");
+                Console.ResetColor();
+
                 return;
             }
             foreach(string item in listToPrint)
