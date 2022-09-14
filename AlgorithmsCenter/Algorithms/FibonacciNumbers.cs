207d6c1511c521153bce5678cebf23ab148ecfc9
dzyzus <dzyzusek@gmail.com>
2022-09-14 18:35:08 +0200

    New features
    * added algorithm template
    * added FibonacciAlgorithm

---
+++ AlgorithmsCenter/Algorithms/FibonacciNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCenter.Algorithms
+{
+    public class FibonacciNumbers
+    {
+        #region Fields and variables
+
+        /// <summary>
+        /// Name of algorithm
+        /// </summary>
+        private string AlgorithmName = "Fibonacci numbers";
+
+        /// <summary>
+        /// Description of algorithm
+        /// </summary>
+        private string DescriptionOfAlgorithm = "First number is equal 0, second 1, every next is sum of two previously";
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Input number to check if it is Fibonacci number
+        /// </summary>
+        protected string NumberToCheck { get; set; }
+        #endregion
+
+        #region methods
+        public void int GetFibonacciNumber()
+        {
+            int fibonacciNumber;
+            Console.WriteLine("Please, insert the number which u want to check:");
+            try
+            {
+                NumberToCheck = Console.ReadLine();
+                NumberToCheck = int.TryParse(NumberToCheck, out fibonacciNumber);
+            }
+        }
+        public void FibonacciAlgorithm
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ AlgorithmsCenter/Templates/AlgorithmsTemplate.cs
@@ -0,0 +1,38 @@
+﻿namespace AlgorithmsCenter.Templates
+{
+    public class AlgorithmsTemplate
+    {
+        #region Fields and variables
+
+        /// <summary>
+        /// Name of algorithm
+        /// </summary>
+        private string AlgorithmName = "Fibonacci numbers";
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Path to plain text
+        /// </summary>
+        protected string PlainTextPath { get; set; }
+
+        /// <summary>
+        /// Path to encrypt text save
+        /// </summary>
+        protected string EncryptTextPath { get; set; }
+
+        /// <summary>
+        /// Path to decrypt text save
+        /// </summary>
+        protected string DecrypteTextPath { get; set; }
+        #endregion
+
+        #region methods
+
+
+
+        #endregion
+    }
+}
